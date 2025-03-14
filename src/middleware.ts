15bbc4be3da64a01f150d0c906477aebf1cf6208
Zaahiid <zahidrsheikh01@gmail.com>
2025-03-14 06:24:58 +0530

    Fixed export issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
-export async function updateSession(request: NextRequest) {
+async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
   })
@@ -63,3 +63,5 @@ export async function updateSession(request: NextRequest) {
 
   return supabaseResponse
 }
+
+export default updateSession;
